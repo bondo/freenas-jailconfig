08eb3841487131869eaf42449fe933deffdfd855
Anders Ingemann <anders@ingemann.de>
2016-04-02 13:38:18 +0200

    sabnzbd

---
+++ roles/sabnzbd/templates/sabnzbd.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# PROVIDE: sabnzbd
+# REQUIRE: DAEMON cleanvar
+# KEYWORD: shutdown
+
+. /etc/rc.subr
+
+sabnzbd_home=/usr/local/libdata/sabnzbd
+sabnzbd_bin=/usr/local/lib/sabnzbd/SABnzbd.py
+
+name=sabnzbd
+rcvar=sabnzbd_enable
+procname=${sabnzbd_bin}
+command=/usr/local/bin/python
+command_args="${sabnzbd_bin} --config-file ${sabnzbd_home}/sabnzbd.ini --server localhost:8080 --daemon"
+sabnzbd_chdir=${sabnzbd_home}
+sabnzbd_user=sabnzbd
+
+load_rc_config $name
+run_rc_command "$@"
