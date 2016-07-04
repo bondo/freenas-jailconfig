8f0ec51bf5d74825dd226ce48f9f02895a0cc48d
Anders Ingemann <anders@ingemann.de>
2016-07-04 23:05:11 +0200

    Use /var/run for rtorrent dtach
    This way we can use the reboot cleanup when rtorrent was not
    shut down cleanly.

--- roles/rtorrent/templates/rtorrent.sh
+++ roles/rtorrent/templates/rtorrent.sh
@@ -13,9 +13,13 @@ name=rtorrent
 rcvar=rtorrent_enable
 procname=${rtorrent_bin}
 command=/usr/local/bin/dtach
-command_args="-n ${rtorrent_home}/rtorrent.dtach ${rtorrent_home}/set_home.sh ${rtorrent_bin}"
+dtach_dir="/var/run/rtorrent/"
+command_args="-n ${dtach_dir}/rtorrent.dtach ${rtorrent_home}/set_home.sh ${rtorrent_bin}"
 rtorrent_chdir=${rtorrent_home}
 rtorrent_user=rtorrent
+argument_precmd=
 
+mkdir $dtach_dir
+chown $rtorrent_user $dtach_dir
 load_rc_config $name
 run_rc_command "$@"
